e32f20aecc9f9c961ea5668f8b5f134f714be30c
FSelanne <72810990+FSelanne@users.noreply.github.com>
2022-12-02 14:08:55 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,39 @@
 #Slap the main program here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------
+
+Standard_Program = MicroControllerSettings(8, 25, 40, 100, 37.5, 20, 100)
+
+'''
+current_moisture = int(input("What is the current moisture in %? "))
+current_temperature = int(input("What is the current temperature in celsius? "))
+current_humidity = int(input("What is the current humidity in %? "))
+current_water_level = int(input("What is the current water level in mm? "))
+Standard_Program.calculate_moisture(current_moisture)
+Standard_Program.calculate_humidity(current_humidity)
+Standard_Program.calculate_temperature(current_temperature)
+Standard_Program.calculate_water_level(current_water_level)
+print(Standard_Program.calculate_water_distribution())
+'''
+
